7559954afd268d48f0e98e2e6a456f4dad8c6026
hyeon <janghyeon6587@gmail.com>
2023-01-02 17:50:37 +0900

    Feat : http client 적용
    * 오픈API : 공공데이터포털 (기상청_전국 해수욕장 날씨 조회서비스)
    * restTemplate 사용
    * jsonObject to Map 적용

--- src/main/java/com/blog/controller/TestController.java
+++ src/main/java/com/blog/controller/TestController.java
@@ -1,22 +1,32 @@
 package com.blog.controller;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.blog.dto.testDto.TestDto;
 import com.blog.mapper.testMapper;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 
@@ -111,6 +121,33 @@ public class TestController {
 
 		return new ResponseEntity("DAO SUCCESS!!!", HttpStatus.OK);
 	}
+	
+	/*
+	 * Test Http
+	 * - https://www.data.go.kr/data/15102239/openapi.do#tab_layer_detail_function
+	 * - 기상청_전국 해수욕장 날씨 조회서비스
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@GetMapping(value = "testHttp")
+	public ResponseEntity<?> testHttp(TestDto dto) throws JSONException, JsonParseException, JsonMappingException, IOException {
+		System.out.println("HTTP RPOCESS...");
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> result = restTemplate.getForEntity(
+				"http://apis.data.go.kr/1360000/BeachInfoservice/getTwBuoyBeach?serviceKey=dwchvY9aMDuvIyEXpAng/VzGX7arSKOVVYWQuukBnQc86OWYba52QhO22gXDo0g7L6eywWqCcwkGEu0Bcz0tvg==&numOfRows=1&pageNo=10&dataType=json&beach_num=1&searchTime=202301021700",
+				String.class);
+
+		// JsonObject to Map
+	    JSONObject jObject = new JSONObject(result.getBody());
+        HashMap<String, Object> map = (HashMap<String, Object>) new ObjectMapper().readValue(jObject.toString(), Map.class);
+
+	    HashMap<String, Object> response = (HashMap<String, Object>) map.get("response");
+	    HashMap<String, Object> body = (HashMap<String, Object>) response.get("body");
+
+		System.out.println("tw = " + ((ArrayList<HashMap<String, Object>>)((HashMap<String, Object>) body.get("items")).get("item")).get(0).get("tw"));
+		return new ResponseEntity("HTTP SUCCESS!!!", HttpStatus.OK);
+	}
+
 
 
 }
